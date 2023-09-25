b98cc4ab521f48289a97f78a221f645e7b269d0b
hgurudu <hari-kumar.gurudu@capgemini.com>
2023-09-25 08:58:48 +0200

    Initial commit

---
+++ src/main/java/com/demo/springbootk8s/Springbootk8sApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootk8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootk8sApplication.class, args);
+	}
+
+}
