5b29d41e0f6c0a1dd533e17f63a6034c91d8db6a
hgurudu <hari-kumar.gurudu@capgemini.com>
2023-09-27 08:35:25 +0200

    Remove Docker compose, .env and sql script files

--- infrastructure/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE "order_users" SUPERUSER;
-CREATE ROLE "order_admins" SUPERUSER;
-CREATE ROLE "order-systemtest" SUPERUSER;
-
-GRANT ALL PRIVILEGES ON DATABASE "postgres-order" TO "order_users";
